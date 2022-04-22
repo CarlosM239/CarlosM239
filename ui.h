f9583ec8d219fed076cf55a68f3b95f8bb5278bf
CarlosM239 <99521449+CarlosM239@users.noreply.github.com>
2022-04-22 16:15:50 -0400

    Create ui.h

---
+++ ui.h
@@ -0,0 +1,13 @@
+#ifndef INHERITANCE_ASSIGNMENT_PIECE_OR_BACKGROUND_STAGE_UI_H
+#define INHERITANCE_ASSIGNMENT_PIECE_OR_BACKGROUND_STAGE_UI_H
+
+#include <iostream>
+
+//function to find out if user wants to continue or not.
+bool continue_or_not();
+
+//function that will get yes/no choice from user
+
+
+
+#endif //INHERITANCE_ASSIGNMENT_PIECE_OR_BACKGROUND_STAGE_UI_H
